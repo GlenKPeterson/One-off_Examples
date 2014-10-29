56ead20a556dc758065c05ebaa6db0f91e20e484
Glen K. Peterson <glen@organicdesign.org>
2014-10-29 08:58:21 -0400

    Create SetInterface.java
    Implementing a common interface is not enough for set/map keys.  The equals() methods of their implementations need to work with each other.  In a language with type erasure, this passes the type checker and breaks at runtime.

---
+++ SetInterface.java
@@ -0,0 +1,65 @@
+/*
+Copyright 2014 Glen Peterson
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+import java.util.HashSet;
+import java.util.Set;
+
+public class SetInterface {
+    static interface Foo {
+        int phooey();
+    }
+    static class Aye implements Foo {
+        private final int phooey;
+        public Aye(int p) { phooey = p; }
+        @Override public int phooey() { return phooey; }
+        @Override public int hashCode() { return phooey; }
+        @Override public boolean equals(Object other) {
+            if (this == other) { return true; }
+            if ( (other == null) ||
+                 !(other instanceof Aye) ||
+                 (this.hashCode() != other.hashCode()) ) {
+                return false;
+            }
+            final Aye that = (Aye) other;
+            return (this.phooey == that.phooey);
+        }
+    }
+
+    static class Boo implements Foo {
+        private final int phooey;
+        public Boo(int p) { phooey = p; }
+        @Override public int phooey() { return phooey; }
+        @Override public int hashCode() { return phooey; }
+        @Override public boolean equals(Object other) {
+            if (this == other) { return true; }
+            if ( (other == null) ||
+                 !(other instanceof Boo) ||
+                 (this.hashCode() != other.hashCode()) ) {
+                return false;
+            }
+            final Boo that = (Boo) other;
+            return (this.phooey == that.phooey);
+        }
+    }
+
+    public static void main(String[] args) {
+        Set<Foo> fooset = new HashSet<>();
+        fooset.add(new Aye(1));
+        fooset.add(new Boo(1));
+        for (Foo foo : fooset) {
+            System.out.println(foo.phooey());
+        }
+    }   
+}
